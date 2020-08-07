6d4108e0ee1a527699b7a632ecbacdf879cef5dd
Trevor Otterson <totterson97@gmail.com>
2020-08-07 13:58:28 -0600

    added in write file for the app.delete method

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,15 @@
 let fs = require("fs");
 let myJson = require("./db/db.json")
+// empty object array to push in new note data
 let notes = [{}]
 
-fs.writeFile("db/db.json", JSON.stringify(myJson), function(err) {
+// writes file into the db.json
+fs.writeFile("db/db.json", JSON.stringify(myJson), function (err) {
     if (err) throw (err)
     console.log('Saved File')
 })
 
+// pulls the information from the dependencies in package.json
 let express = require("express")
 let path = require("path")
 
@@ -15,43 +18,75 @@ let app = express()
 let PORT = process.env.PORT || 3000
 
 // Sets up the Express application to handle data parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Links all the files to the server.js file
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
-app.get("/assets/js/index.js", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+app.get("/assets/js/index.js", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"))
 });
-app.get("/assets/css/styles.css", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
+app.get("/assets/css/styles.css", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
 });
 
-// api link
-app.get("/api/notes", function(req, res) {
-    fs.readFile(__dirname + "/db/db.json", function(err,data) {
-    if (err) throw (err)
-    let notes = JSON.parse(data)
-    return res.json(notes)
+// API link
+// displays all notes
+app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+        if (err) throw err
+        let notes = JSON.parse(data)
+        return res.json(notes)
     })
 });
 
-app.post("/api/characters", function(req, res) {
+// posts data to the notes in the REST API
+app.post("/api/notes", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newCharacter = req.body;
+    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+        if (err) throw err
+        let notes = JSON.parse(data)
+        let new_notes = req.body
+        // pushes new_notes into notes object array
+        new_notes.id = new_notes.title
+        notes.push(new_notes)
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+        // writes file into db.json
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
+            if (err) throw err
+            console.log(new_notes)
+            res.json(new_notes)
+        })
+    })
+})
 
-    console.log(newCharacter);
+// gives each note a unique `id` when it's saved, and deletes notes with given `id` on command
+app.delete("/api/notes/:id", function (req, res) {
+    // uses params to create individual note `id`
+    let note_id = req.params.id
+    // collects notes from the db
+    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+        if (err) throw err
+        let notes = JSON.parse(data)
+        let note_filter = notes.filter((note) => {return note.id !== note_id})
+        
+        // writes new note data into db.json
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(note_filter), function (err, data) {
+            if (err) throw err
+        })
+    })
+})
+// Using a RegEx Pattern to remove spaces from newCharacter
+// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-    characters.push(newCharacter);
+console.log(newCharacter);
 
-    res.json(newCharacter);
+characters.push(newCharacter);
+
+res.json(newCharacter);
 });
 
 app.listen(PORT, function () {
