f2c297e5e374e63c7d216433b019ae62e3d84727
Trevor Otterson <totterson97@gmail.com>
2020-08-08 08:28:10 -0600

    made code more dry

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ let myJson = require("./db/db.json")
 // empty object array to push in new note data
 let notes = [{}]
 
-// writes file into the db.json
-fs.writeFile("db/db.json", JSON.stringify(myJson), function (err) {
+// writes file into the db.json *
+fs.writeFile("db/db.json", JSON.stringify(myJson), (err) => {
     if (err) throw (err)
     console.log('Saved File')
 })
@@ -25,20 +25,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Links all the files to the server.js file
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
-app.get("/assets/js/index.js", function (req, res) {
+app.get("/assets/js/index.js", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/assets/js/index.js"))
 })
-app.get("/assets/css/styles.css", function (req, res) {
+app.get("/assets/css/styles.css", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"))
 })
 
 // API link
 // displays all notes
-app.get("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+app.get("/api/notes", (req, res) => {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
         if (err) throw err
         let notes = JSON.parse(data)
         return res.json(notes)
@@ -46,9 +46,9 @@ app.get("/api/notes", function (req, res) {
 })
 
 // posts data to the notes in the REST API
-app.post("/api/notes", function (req, res) {
+app.post("/api/notes", (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
-    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
         if (err) throw err
         let notes = JSON.parse(data)
         let new_notes = req.body
@@ -57,7 +57,7 @@ app.post("/api/notes", function (req, res) {
         notes.push(new_notes)
 
         // writes file into db.json
-        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
             if (err) throw err
             console.log(new_notes)
             res.json(new_notes)
@@ -66,17 +66,17 @@ app.post("/api/notes", function (req, res) {
 })
 
 // gives each note a unique `id` when it's saved, and deletes notes with given `id` on command
-app.delete("/api/notes/:id", function (req, res) {
+app.delete("/api/notes/:id", (req, res)  =>{
     // uses params to create individual note `id`
     let note_id = req.params.id
     // collects notes from the db
-    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
         if (err) throw err
         let notes = JSON.parse(data)
         let note_filter = notes.filter((note) => { return note.id !== note_id })
 
         // writes new note data into db.json
-        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(note_filter), function (err, data) {
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(note_filter), (err, data) => {
             if (err) throw err
             console.log(note_filter)
             res.json(note_filter)
@@ -85,11 +85,11 @@ app.delete("/api/notes/:id", function (req, res) {
 })
 
 // joins file to the index.html and gets all notes
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 // listens on PORT 3000
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log("Application is listening on PORT " + PORT);
 })
